e9052360a8f33bd8585cdef0b015ac1d586d4099
UNIT Enterprise Systems <86251901+UNIT-System5@users.noreply.github.com>
2021-07-16 18:27:10 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function ProfileClick() {
+    const clickProfile = document.querySelector('.signin');
+    clickProfile.classList.toggle('active');
+}
+
+function MenuClick() {
+    const clickMenu = document.querySelector('.menu');
+    clickMenu.classList.toggle('active');
+}
