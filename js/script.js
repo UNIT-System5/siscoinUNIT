0f63c7e984e1dac104d5ac3f08f1ecf6b113c012
UNIT Enterprise Systems <86251901+UNIT-System5@users.noreply.github.com>
2021-08-19 21:55:07 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function ProfileClick() {
-    const clickProfile = document.querySelector('.signin');
-    clickProfile.classList.toggle('active');
-}
-
-function MenuClick() {
-    const clickMenu = document.querySelector('.menu');
-    clickMenu.classList.toggle('active');
-}
