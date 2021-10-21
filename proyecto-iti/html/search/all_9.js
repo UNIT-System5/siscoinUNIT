166df1d9f65be08774ad7a8a6ec4af4e3ea9390a
UNIT Enterprise Systems <86251901+UNIT-System5@users.noreply.github.com>
2021-10-21 18:47:54 -0300

    Delete proyecto-iti directory

--- proyecto-iti/html/search/all_9.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['sqlcl_0',['sqlCL',['../class_d_b_login_connect.html#a7ac6893f7e27646b9da5fb1ac7600cee',1,'DBLoginConnect']]],
-  ['sqlcu_1',['sqlCU',['../class_d_b_user_connect.html#af1dee401cb844f12b3771b1fc9f51e3c',1,'DBUserConnect']]]
-];
--- proyecto-iti/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['controller_2ephp_0',['controller.php',['../controller_8php.html',1,'']]]
-];
