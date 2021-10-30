51219738da93d6f8208d8fb88bb2ef4e437e2e1c
Gian Luca <55563130+GLPG35@users.noreply.github.com>
2021-10-30 13:00:47 -0300

    Delete proyecto-iti directory

--- proyecto-iti/html/search/all_a.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['user_0',['User',['../class_user.html',1,'']]],
-  ['users_2ephp_1',['users.php',['../admin_2users_8php.html',1,'(Global Namespace)'],['../informatica_2users_8php.html',1,'(Global Namespace)']]]
-];
--- proyecto-iti/views/js/shop.js
+++
@@ -1,8 +0,0 @@
-import { header, profilePic, homeRedir } from './modules/header.js';
-
-header();
-homeRedir();
-
-$(window).on('load', () => {
-    profilePic();
-});
